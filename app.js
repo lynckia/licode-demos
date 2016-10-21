d28fa1dc2d1f665018c8e8149e5cc982eeabe5b0
Lourdesmarco <lourdesmarco.design@gmail.com>
2016-10-21 14:58:07 +0200

    styling: catedra telefonica layout

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var config = require('./config')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
